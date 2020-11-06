e2b7fa0a54db57b42c00807da0c09779fb670b34
Emily Plummer <emplums@github.com>
2020-11-06 15:00:14 -0800

    if props.sx is undefined us an empty object

--- src/sx.js
+++ src/sx.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 import css from '@styled-system/css'
 
 export const sx = props => css(props.sx)
-export const mergeCustomStyles = (base, override) => Object.assign(base, override)
+export const mergeCustomStyles = (base = {}, override = {}) => Object.assign(base, override)
 
 sx.propTypes = {
   sx: PropTypes.object
