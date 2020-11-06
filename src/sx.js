dbdf7340042438fcf9ec44a9fa1f835413d6b13e
Emily Plummer <emplums@github.com>
2020-11-06 15:02:21 -0800

    add comment for mergeCustomStyles

--- src/sx.js
+++ src/sx.js
@@ -2,6 +2,8 @@ import PropTypes from 'prop-types'
 import css from '@styled-system/css'
 
 export const sx = props => css(props.sx)
+// merges user defined sx props with sx props defined in a component implementation
+// the second argument/style object passed in will take precendence over the first
 export const mergeCustomStyles = (base = {}, override = {}) => Object.assign(base, override)
 
 sx.propTypes = {
