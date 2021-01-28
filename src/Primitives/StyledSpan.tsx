bee00e5f5f84fa04bb49474c905914aafdb5b88c
Clay Miller <clay@smockle.com>
2021-01-28 15:59:29 -0500

    fix: Add COMMON and WIDTH

--- src/Primitives/StyledSpan.tsx
+++ src/Primitives/StyledSpan.tsx
@@ -1,13 +1,15 @@
 import styled from 'styled-components'
+import {width, WidthProps} from 'styled-system'
 import {COMMON, SystemCommonProps} from '../constants'
 import sx, {SxProp} from '../sx'
 
-export interface StyledSpanProps extends SystemCommonProps, SxProp {
+export interface StyledSpanProps extends WidthProps, SystemCommonProps, SxProp {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [key: string]: any
 }
 
 export const StyledSpan = styled.span<StyledSpanProps>`
   ${COMMON}
+  ${width}
   ${sx}
 `
--- src/ProgressBar/Bar.tsx
+++ src/ProgressBar/Bar.tsx
@@ -1,14 +1,15 @@
 import React, {PropsWithChildren} from 'react'
 import {SystemCommonProps} from '../constants'
 import {StyledSpan} from '../Primitives/StyledSpan'
+import {SxProp} from '../sx'
 
-export interface BarProps extends SystemCommonProps {
+export interface BarProps extends SystemCommonProps, SxProp {
   progress?: string | number
 }
 
-export function Bar({progress, children, ...props}: PropsWithChildren<BarProps>): JSX.Element {
+export function Bar({sx, progress, children, ...props}: PropsWithChildren<BarProps>): JSX.Element {
   return (
-    <StyledSpan {...props} sx={{width: progress ? `${progress}%` : 0}}>
+    <StyledSpan {...props} sx={{width: progress ? `${progress}%` : 0, ...sx}}>
       {children}
     </StyledSpan>
   )
